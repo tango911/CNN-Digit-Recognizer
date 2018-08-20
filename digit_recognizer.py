acad778cea2892e1d45a504da15b9acf5c5fcc32
Mohit Chaturvedi <mohitcck@gmail.com>
2018-08-20 13:45:50 +0530

    flask support functions

---
+++ digit_recognizer.py
@@ -0,0 +1,40 @@
+import  re
+import os
+from skimage.transform import resize
+from PIL import Image
+
+from keras.models import load_model
+import base64
+import numpy as np
+import tensorflow as tf
+
+global model
+
+
+model = load_model(os.path.join('digit_clfr.h5'))
+graph = tf.get_default_graph()
+
+def convertImage(imgData1):
+
+    imgstr = re.search(b'base64,(.*)', imgData1).group(1)
+    with open('output.png','wb') as output:
+        output.write(base64.decodebytes(imgstr))
+
+
+def digit(imgData):
+    convertImage(imgData)
+
+    x=  Image.open('output.png').convert('L')
+    x = np.invert(x)
+    x = resize(x,(28,28),order=1, mode='constant', cval=0, clip=False, preserve_range=True)
+    x= x.astype(int)
+    x = x.reshape(1,28,28,1)
+
+    with graph.as_default():
+        y_pred=model.predict(x)
+
+    for i in range(len(y_pred[0])):
+
+        if y_pred[0][i]==1:
+            m=i
+    return m
